7577e12c3949304d7f194179e486bab3eb906d83
Shriram <shriramd12@gmail.com>
2026-02-19 23:24:42 +0530

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Shriram Dharmadhikari on 19/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShoppingListApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
